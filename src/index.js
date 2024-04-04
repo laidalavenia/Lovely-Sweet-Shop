d3aad544e18b0926586f9f7267a6462a2b35f9d7
laidalavenia <laida.vanida@gmail.com>
2024-04-05 06:44:48 +0700

    rename root

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-const root = document.getElementById("docs");
+const root = document.getElementById("root");
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <App />
